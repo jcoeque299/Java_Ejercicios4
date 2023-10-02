3e82590bb030d331079f633b7fe506343e9896b3
vjimcor955 <vjimcor955@g.educaand.es>
2023-10-02 10:09:55 +0200

    Ejercicios 8 terminado

---
+++ src/ejercicio8.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ejercicio8 {
+    public static void main(String[] args){
+        Random rand = new Random();
+        List<List> apuestas = new ArrayList<>(List.of());
+        List<String> resultadosPosibles = List.of("1", "1", "1", "X", "X", "2");
+        List<String> resultadosPosiblesPleno = List.of("0","1","2","M");
+        for (int cont = 0; apuestas.size() < 14; cont++) {
+            apuestas.add(List.of(resultadosPosibles.get(rand.nextInt(6)),resultadosPosibles.get(rand.nextInt(6)),resultadosPosibles.get(rand.nextInt(6))));
+            //Para cada apuesta, se crea una lista de 3 apuestas, hasta llegar a los 15 partidos
+        }
+        apuestas.add(List.of(resultadosPosiblesPleno.get(rand.nextInt(4)),resultadosPosiblesPleno.get(rand.nextInt(4)),resultadosPosiblesPleno.get(rand.nextInt(4))));
+        //Añade las 3 ultimas apuestas para el pleno
+        for (List apuesta: apuestas) {
+            System.out.println(apuesta.toString());
+        }
+    }
+}
