3868ae22e1ef874895a5a445af4a2964a323d73e
Judith <jcoeque299@g.educaand.es>
2023-10-02 09:34:39 +0200

    Otra correcion en el ejercicio 7

--- src/ejercicio7.java
+++ src/ejercicio7.java
@@ -9,10 +9,10 @@ public class ejercicio7 {
         List<String> resultadosPosibles = List.of("1", "2", "X");
         List<String> resultadosPosiblesPleno = List.of("0","1","2","M");
         for (int cont = 0; apuestas.size() < 14; cont++) {
-            apuestas.add(List.of(resultadosPosibles.get(rand.nextInt(2)),resultadosPosibles.get(rand.nextInt(2)),resultadosPosibles.get(rand.nextInt(2))));
+            apuestas.add(List.of(resultadosPosibles.get(rand.nextInt(3)),resultadosPosibles.get(rand.nextInt(3)),resultadosPosibles.get(rand.nextInt(3))));
             //Para cada apuesta, se crea una lista de 3 apuestas, hasta llegar a los 15 partidos
         }
-        apuestas.add(List.of(resultadosPosiblesPleno.get(rand.nextInt(3)),resultadosPosiblesPleno.get(rand.nextInt(3)),resultadosPosiblesPleno.get(rand.nextInt(3))));
+        apuestas.add(List.of(resultadosPosiblesPleno.get(rand.nextInt(4)),resultadosPosiblesPleno.get(rand.nextInt(4)),resultadosPosiblesPleno.get(rand.nextInt(4))));
         //Añade las 3 ultimas apuestas para el pleno
         for (List apuesta: apuestas) {
             System.out.println(apuesta.toString());
